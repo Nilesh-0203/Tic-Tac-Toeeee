449d920fb5269f2d521faf4e7154deede13761c4
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2023-12-24 12:44:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,85 +1,91 @@
-let boxes=document.querySelectorAll(".box");
-let rst_btn=document.querySelector("#rst-button");
-let new_btn=document.querySelector("#new-button");
-let mesg_cont=document.querySelector(".msg-container");
-let msg=document.querySelector("#msg");
-let count=0;
-let trun=true;
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
-const winning_pattern=[
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8]
-]
- 
+let turnO = true; //playerX, playerO
+let count = 0; //To Track Draw
 
-boxes.forEach((box) => {
-    box.addEventListener("click",()=>{
-        if(trun){
-            console.log("Player 1 done (X) ");
-            box.innerText="X";
-            trun=false;
-        }
-        else{
-            console.log("Player 2 done (O) ");
-            box.innerText="O";
-            trun=true;
-        }
-        box.disabled=true;
-        ++count;
-        checkWinner();
-    });
-});
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
 
-const resetGame=()=>{
-    enableBoxes();
-    mesg_cont.classList.add("hide");
-}
-
-const disableBoxes = () => {
-    for(let box of boxes){
-        box.disabled=true;
-    }
-}
-const enableBoxes = () => {
-    for(let box of boxes){
-        box.disabled=false;
-        box.innerText="";
-    }
-}
-const showWinner=(pos1Val)=>{
-    msg.innerText=`Congratulations, Winner is ${pos1Val}`;
-    mesg_cont.classList.remove("hide");
-    disableBoxes();
-}
-
-const drawFun=()=>{
-    msg.innerText=`Game is Draw`;
-    mesg_cont.classList.remove("hide");
-}
-const checkWinner=()=>{
-   for(let pattern of winning_pattern){
-    let pos1Val=boxes[pattern[0]].innerText;
-    let pos2Val=boxes[pattern[1]].innerText;
-    let pos3Val=boxes[pattern[2]].innerText;
-
-    if(pos1Val !="" && pos2Val!="" && pos3Val!=""){
-        if(pos1Val===pos2Val && pos2Val===pos3Val){
-            console.log(`Winner is ${pos1Val}`);
-            count=0;
-            showWinner(pos1Val);
-        }
-        if(count==9){
-            drawFun();
-        }
-    }
-   }
+const resetGame = () => {
+  turnO = true;
+  count = 0;
+  enableBoxes();
+  msgContainer.classList.add("hide");
 };
 
-new_btn.addEventListener("click",resetGame);
-rst_btn.addEventListener("click",resetGame);
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      //playerO
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      //playerX
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+    count++;
+
+    let isWinner = checkWinner();
+
+    if (count === 9 && !isWinner) {
+      gameDraw();
+    }
+  });
+});
+
+const gameDraw = () => {
+  msg.innerText = `Game was a Draw.`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        showWinner(pos1Val);
+        return true;
+      }
+    }
+  }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
